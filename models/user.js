37c2dd1273d4e74e2e07e4d3dedc00d94f295227
shivam <chotu.kv@gmail.com>
2017-08-13 23:41:19 +0530

    Schema improved

--- models/user.js
+++ models/user.js
@@ -23,13 +23,13 @@ var userSchema = new Schema({
 	middle_name		: {type : String ,default :''},
 	last_name 		: {type : String ,default :''},
 	aadhar_number 	: {type : Number,required : true,unique : true,
-				min :[1000000000000000,'NOT VALID NUMBER RETRY!!'],
-				max :[9999999999999999,'NOT VALID NUMBER!! RETRY'],                         
-				validate :{
-					validator : Number.isInteger,
-					message : '{VALUE} is not a integer value',
-				}
-			},
+		min :[100000000000,'NOT VALID NUMBER RETRY!!'],
+		max :[999999999999,'NOT VALID NUMBER!! RETRY'],                         
+		validate :{
+			validator : Number.isInteger,
+			message : '{VALUE} is not a integer value',
+		}
+	},
 	user_name 		: {type : String,required : true,unique : true},	
 	email     		: {work : mongoose.SchemaTypes.Email,required:'true',home : mongoose.SchemaTypes.Email,unique : true,required : 'Email address is required'},
 	password 		: {type : String , required : true },
@@ -38,35 +38,37 @@ var userSchema = new Schema({
 	profession 		: {type : String,required : true},
 	isBanned 		: {type : Boolean,default : false},
 	group			: {type : Number,default : 8},
-					  {timestamps: true}	
+	{timestamps: true},
+	dateofbirth 		:{type : Date , required : true},
+	gender 			:{type : String , required : true}	
 });
 
 // Hashing password and protecting it from rainbow attack i.e maximum login limit
 userSchema.pre(save,function(next){
-		var user = this;
-		if(!user.isModified('password')) return next();
-		//generation of a salt 
-		bcrypt.genSalt(SALT_WORK_FACTOR,function(err,salt){
-				if(err) 
-					return next(err);
-				// hash password using new salt
-				bcrypt.hash(user.password , salt , function (err , hash){
-						if(err) 
-							return next (err);
-						//overide the passwerd with hashed one
-						user.password = hash;
-						next();
-						});
-				});
-
+	var user = this;
+	if(!user.isModified('password')) return next();
+	//generation of a salt 
+	bcrypt.genSalt(SALT_WORK_FACTOR,function(err,salt){
+		if(err) 
+			return next(err);
+		// hash password using new salt
+		bcrypt.hash(user.password , salt , function (err , hash){
+			if(err) 
+				return next (err);
+			//overide the passwerd with hashed one
+			user.password = hash;
+			next();
 		});
+	});
+
+});
 
 UserSchema.methods.comparePassword =  function(candidatePassword , cb){
 	bcrypt.compare(candidatePassword,this.password,function(err,isMatch){
-			if(err)
-				return cb(err);
-			cb(null,isMatch);
-			})
+		if(err)
+			return cb(err);
+		cb(null,isMatch);
+	})
 }
 
 module.exports = mongoose.model(user , userSchema);
