9765d2aa3b703362a2e8fef95afde1a49769c097
root <root@localhost.localdomain>
2017-08-13 22:31:02 +0530

    more indenting in user.js

--- models/user.js
+++ models/user.js
@@ -9,10 +9,10 @@ SALT_WORK_FACTOR = 10;
 /*Schema to store the addresses of the users*/
 var addressSchema =  new Schema({
 	address1 		: {type : String,required : true;},
-    address2 		: {type : String,default : ''},
-    city 			: {type : String,required : true},
-    district 		: {type : String,required : true},
-    state 			: {type : String,required : true},
+	address2 		: {type : String,default : ''},
+	city 			: {type : String,required : true},
+	district 		: {type : String,required : true},
+	state 			: {type : String,required : true},
 })
 
 /*A user schema to store all its details*/
@@ -20,47 +20,49 @@ var userSchema = new Schema({
 	first_name		: {type : String ,default :'',required:true},
 	middle_name		: {type : String ,default :''},
 	last_name 		: {type : String ,default :''},
-    aadhar_number 	: {type : Number,required : true,unique : true,
-                        min :[1000000000000000,'NOT VALID NUMBER RETRY!!'],
-                        max :[9999999999999999,'NOT VALID NUMBER!! RETRY'],                         
-                        validate :{
-                                validator : Number.isInteger,
-                                message : '{VALUE} is not a integer value',
-                        	}
-                		},
-    user_name 		: {type : String,required : true,unique : true},	
-    email     		: {work : mongoose.SchemaTypes.Email,required:'true',home : mongoose.SchemaTypes.Email,unique : true,required : 'Email address is required'},
-    password 		: {type : String , required : true },
-    mobile 			: {type : Number,required : true,unique; true},
-    address 		: [addressSchema],
-    profession 		: {type : String,required : true},
-    isBanned 		: {type : Boolean,default : false}	
+	aadhar_number 	: {type : Number,required : true,unique : true,
+				min :[1000000000000000,'NOT VALID NUMBER RETRY!!'],
+				max :[9999999999999999,'NOT VALID NUMBER!! RETRY'],                         
+				validate :{
+					validator : Number.isInteger,
+					message : '{VALUE} is not a integer value',
+				}
+			},
+	user_name 		: {type : String,required : true,unique : true},	
+	email     		: {work : mongoose.SchemaTypes.Email,required:'true',home : mongoose.SchemaTypes.Email,unique : true,required : 'Email address is required'},
+	password 		: {type : String , required : true },
+	mobile 			: {type : Number,required : true,unique; true},
+	address 		: [addressSchema],
+	profession 		: {type : String,required : true},
+	isBanned 		: {type : Boolean,default : false}	
 });
 
 // Hashing password and protecting it from rainbow attack i.e maximum login limit
 userSchema.pre(save,function(next){
-        var user = this;
-        if(!user.isModified('password')) return next();
-        //generation of a salt 
-        bcrypt.genSalt(SALT_WORK_FACTOR,function(err,salt){
-                if(err) return next(err);
-                // hash password using new salt
-                bcrypt.hash(user.password , salt , function (err , hash){
-                        if(err) return next (err);
-                        //overide the passwerd with hashed one
-                        user.password = hash;
-                        next();
-                });
-        });
-        
-});
+		var user = this;
+		if(!user.isModified('password')) return next();
+		//generation of a salt 
+		bcrypt.genSalt(SALT_WORK_FACTOR,function(err,salt){
+				if(err) 
+					return next(err);
+				// hash password using new salt
+				bcrypt.hash(user.password , salt , function (err , hash){
+						if(err) 
+							return next (err);
+						//overide the passwerd with hashed one
+						user.password = hash;
+						next();
+						});
+				});
+
+		});
 
 UserSchema.methods.comparePassword =  function(candidatePassword , cb){
 	bcrypt.compare(candidatePassword,this.password,function(err,isMatch){
-		if(err)
-			return cb(err);
-		cb(null,isMatch);
-	})
+			if(err)
+				return cb(err);
+			cb(null,isMatch);
+			})
 }
 
 module.exports = mongoose.model(user , userSchema);
